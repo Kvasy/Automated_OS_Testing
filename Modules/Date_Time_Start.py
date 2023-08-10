063d2adbc7c4bc7fffef365c5aa082bf2d4910fc
Kvasy <41273287+Kvasy@users.noreply.github.com>
2023-08-10 09:44:46 -0400

    Delete Modules directory

--- Modules/Date_Time_Start.py
+++
@@ -1,6 +0,0 @@
-from datetime import datetime
-
-def Date_Time_Start():
-    current_dateTime = datetime.now()
-    print('Date: ', current_dateTime.year, '/', current_dateTime.month, '/', current_dateTime.day)
-    print ('Time:', current_dateTime.hour, ':', current_dateTime.minute, ':', current_dateTime.second)
